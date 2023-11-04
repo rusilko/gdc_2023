64112079fb9f052a06f2bce1772d3bec6888b7d9
Wiktor Gut <94977771+Guccio163@users.noreply.github.com>
2023-11-04 14:25:37 +0100

    second test

--- game.rb
+++ game.rb
@@ -3,7 +3,14 @@ require 'minitest/pride'
 require 'set'
 
 class Cell
-  def initialize
+  def initialize(x, y, isAlive)
+    @x = x
+    @y = y
+    @isAlive = isAlive
+  end
+
+  def alive?
+    @isAlive
   end
 end
 
@@ -17,6 +24,12 @@ class GameTest < MiniTest::Unit::TestCase
     c = Cell.new(0,0,true)
 
     assert c.alive?
+  end
+
+  def second_test
+    c = Cell.new(1,1, true)
+
+    assert c.neighbours, [Cell.new(0,0,true), Cell.new(0,1,true), Cell.new(0,2,true), Cell.new(1,0,true), Cell.new(1,2,true), Cell.new(2,0,true), Cell.new(2,1,true), Cell.new(2,2,true)]
   end
 end
 
