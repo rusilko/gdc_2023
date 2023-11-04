02b2f0996e6ff6b9d5d8f149f6b55709c44eb858
rusilko <tomek.rusilko@lunarlogic.io>
2023-11-04 14:47:02 +0100

    3

--- game.rb
+++ game.rb
@@ -2,15 +2,48 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'set'
 
+class Game
+
+end
+
 class Cell
-  def initialize(x, y, isAlive)
+  def initialize(x, y, isAlive = false)
     @x = x
     @y = y
     @isAlive = isAlive
   end
 
+  def x
+    @x
+  end
+
+  def y
+    @y
+  end
+
   def alive?
     @isAlive
+  end
+
+  def neighbours
+    ret_val = Set.new
+    [-1,0,1].each do |i|
+      [-1,0,1].each do |j|
+        next if i == 0 && j == 0
+        new_cell = Cell.new(@x+i, @y+j)
+        ret_val.add(new_cell)
+      end
+    end
+
+    ret_val
+  end
+
+  def eql?(other)
+    @x == other.x && @y == other.y
+  end
+
+  def hash
+    [@x, @j].hash
   end
 end
 
@@ -26,10 +59,16 @@ class GameTest < MiniTest::Unit::TestCase
     assert c.alive?
   end
 
-  def second_test
+  def test_second
     c = Cell.new(1,1, true)
+    expected = Set.new([Cell.new(0,0,true), Cell.new(0,1,true), Cell.new(0,2,true), Cell.new(1,0,true), Cell.new(1,2,true), Cell.new(2,0,true), Cell.new(2,1,true), Cell.new(2,2,true)])
+    result = c.neighbours
 
-    assert c.neighbours, [Cell.new(0,0,true), Cell.new(0,1,true), Cell.new(0,2,true), Cell.new(1,0,true), Cell.new(1,2,true), Cell.new(2,0,true), Cell.new(2,1,true), Cell.new(2,2,true)]
+    assert_equal c.neighbours, Set.new([Cell.new(0,0,true), Cell.new(0,1,true), Cell.new(0,2,true), Cell.new(1,0,true), Cell.new(1,2,true), Cell.new(2,0,true), Cell.new(2,1,true), Cell.new(2,2,true)])
+  end
+
+  def test_game
+    g = Game.new([
   end
 end
 
